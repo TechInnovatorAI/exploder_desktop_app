4cec7d549fd4a9fd99da171090a4305fc43773f8
TechInnovatorAI <masatotanaka0219@gmail.com>
2025-07-08 07:12:40 +0900

    feature : add triangleTool, roundedRectTool

---
+++ PointData.cs
@@ -0,0 +1,8 @@
+﻿namespace Exploder
+{
+    public class PointData
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}                                                   
